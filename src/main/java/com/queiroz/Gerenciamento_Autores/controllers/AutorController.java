92e03eb3fd51ce4d8319b66dd5b2e0bccaa4e7d6
ferqueiroz <ferqueiroz2205@gmail.com>
2026-03-18 22:21:29 -0300

    Finalizado a parte de requisições

--- src/main/java/com/queiroz/Gerenciamento_Autores/controllers/AutorController.java
+++ src/main/java/com/queiroz/Gerenciamento_Autores/controllers/AutorController.java
@@ -1,12 +1,44 @@
 package com.queiroz.Gerenciamento_Autores.controllers;
 
+import com.queiroz.Gerenciamento_Autores.models.AutorModel;
 import com.queiroz.Gerenciamento_Autores.services.AutorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/autores")
 public class AutorController {
     @Autowired
     private AutorService autorService;
+
+    @PostMapping
+    public ResponseEntity<AutorModel> criar(AutorModel autorModel) {
+        return ResponseEntity.status(201).body(autorService.criar(autorModel));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AutorModel>> listar() {
+        return ResponseEntity.ok().body(autorService.listar());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AutorModel> buscar(@PathVariable Long id) {
+        Optional<AutorModel> livro = autorService.buscar(id);
+
+        if (livro.isPresent())
+            return ResponseEntity.ok().body(livro.get());
+        else
+            return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<AutorModel> deletar(@PathVariable Long id) {
+        autorService.excluir(id);
+
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/queiroz/Gerenciamento_Autores/services/AutorService.java
+++ src/main/java/com/queiroz/Gerenciamento_Autores/services/AutorService.java
@@ -1,11 +1,31 @@
 package com.queiroz.Gerenciamento_Autores.services;
 
+import com.queiroz.Gerenciamento_Autores.models.AutorModel;
 import com.queiroz.Gerenciamento_Autores.repositories.AutorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AutorService {
     @Autowired
     private AutorRepository autorRepository;
+
+    public AutorModel criar(AutorModel autorModel) {
+        return autorRepository.save(autorModel);
+    }
+
+    public List<AutorModel> listar() {
+        return autorRepository.findAll();
+    }
+
+    public Optional<AutorModel> buscar(Long id) {
+        return autorRepository.findById(id);
+    }
+
+    public void excluir(Long id) {
+        autorRepository.deleteById(id);
+    }
 }
